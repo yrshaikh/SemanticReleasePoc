e0ec4ad18f58e03e729251c450b37f405d68ddc2
Yasser shaikh <yrshaikh.mail@gmail.com>
2021-01-31 13:35:35 +0700

    style: cleanup

--- release.config.js
+++ release.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  branches: ['main'],
-  prepare: [
-        {
-            path: "@semantic-release/exec",
-            cmd: "echo prepare"
-        }
-    ],
+    branches: ['main'],
+    prepare: [{
+        path: "@semantic-release/exec",
+        cmd: "echo prepare"
+    }],
     publish: [
-        "@semantic-release/github",
-        {
+        "@semantic-release/github", {
             path: "@semantic-release/exec",
             cmd: "echo publish
         }
